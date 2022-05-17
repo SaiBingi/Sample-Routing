2245f7767b81c198282ae34389072482d2510f9b
Sai Bingi <saibingi1208@gmail.com>
2022-05-17 08:34:57 +0000

    Sample Routing Solution

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const Home = () => (
+  <div className="component-display">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+      alt="home"
+      className="component-image"
+    />
+    <h1 className="component-display-text">Home</h1>
+  </div>
+)
+
+export default Home
